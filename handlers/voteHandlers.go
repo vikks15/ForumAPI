ae766ab77bcfddf1863261bcb7d5bf6bab11782c
vikks15 <lva9@mail.ru>
2019-01-06 01:47:30 +0300

    Votes bulk insert

--- handlers/voteHandlers.go
+++ handlers/voteHandlers.go
@@ -11,6 +11,7 @@ import (
 )
 
 func (env *Env) CreateVote(w http.ResponseWriter, r *http.Request) {
+	var err error
 	vars := mux.Vars(r)
 	var newVote structs.Vote
 	var votedThread structs.Thread
@@ -27,6 +28,15 @@ func (env *Env) CreateVote(w http.ResponseWriter, r *http.Request) {
 		fmt.Print("\n")
 		return
 	}
+	defer tx.Rollback()
+
+	_, err = tx.Exec("SET LOCAL synchronous_commit = OFF")
+
+	if err != nil {
+		fmt.Print(err)
+		fmt.Print("\n")
+		return
+	}
 
 	if _, err := strconv.Atoi(vars["slug_or_id"]); err == nil {
 		sqlStatement = `SELECT id FROM thread WHERE id = $1`
@@ -34,14 +44,13 @@ func (env *Env) CreateVote(w http.ResponseWriter, r *http.Request) {
 		sqlStatement = `SELECT id FROM thread WHERE slug = $1`
 	}
 
-	row := env.DB.QueryRow(sqlStatement, vars["slug_or_id"])
+	row := tx.QueryRow(sqlStatement, vars["slug_or_id"])
 	scanErr := row.Scan(&newVote.ThreadId)
 
 	if scanErr != nil {
 		fmt.Print("\n CreateVote threadNotFound:")
 		fmt.Print(scanErr)
 		fmt.Print("\n")
-		tx.Rollback()
 
 		w.WriteHeader(http.StatusNotFound)
 		errorMsg := map[string]string{"message": "Can't find thread by slug_or_id " + vars["slug_or_id"] + "\n"}
@@ -50,16 +59,15 @@ func (env *Env) CreateVote(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	//---------------------------------------------
 	sqlStatement = `INSERT INTO vote (nickname, voice, threadId)
-					VALUES ($1,$2,$3)
-					ON CONFLICT (nickname, threadId) DO UPDATE
-					SET voice = $2`
-	_, err = env.DB.Exec(sqlStatement, newVote.Nickname, newVote.Voice, newVote.ThreadId)
+	VALUES ($1,$2,$3)
+	ON CONFLICT (nickname, threadId) DO UPDATE
+	SET voice = $2`
+	_, err = tx.Exec(sqlStatement, newVote.Nickname, newVote.Voice, newVote.ThreadId)
 
 	if err != nil {
 		fmt.Print(err)
-		tx.Rollback()
-
 		w.WriteHeader(http.StatusNotFound)
 		errorMsg := map[string]string{"message": "Can't find user or thread\n"}
 		response, _ := json.Marshal(errorMsg)
@@ -68,18 +76,17 @@ func (env *Env) CreateVote(w http.ResponseWriter, r *http.Request) {
 	}
 
 	sqlStatement = `UPDATE thread SET votes = 
-					(select sum(voice) from vote where threadId = $1)
-					where id = $1`
+		(select sum(voice) from vote where threadId = $1)
+		where id = $1`
 
-	_, err = env.DB.Exec(sqlStatement, newVote.ThreadId)
+	_, err = tx.Exec(sqlStatement, newVote.ThreadId)
 
 	if err != nil {
 		fmt.Print("update votedThreadErr: ")
 		fmt.Print(err)
 		fmt.Print("\n")
-		tx.Rollback()
 	} else {
-		row := env.DB.QueryRow("SELECT id, title, author, forum, message, votes, slug, created FROM vote JOIN thread ON (threadId = id) where id = " + strconv.Itoa(newVote.ThreadId))
+		row := tx.QueryRow("SELECT id, title, author, forum, message, votes, slug, created FROM vote JOIN thread ON (threadId = id) where id = " + strconv.Itoa(newVote.ThreadId))
 		scanErr := row.Scan(&votedThread.Id, &votedThread.Title, &votedThread.Author, &votedThread.Forum, &votedThread.Message, &votedThread.Votes, &votedThread.Slug, &votedThread.Created)
 
 		if scanErr != nil {
